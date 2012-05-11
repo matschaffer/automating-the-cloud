2ca17352b6b23ec512df34ae0c959b6a91289304
Mat Schaffer <mat@schaffer.me>
2012-05-11 14:58:50 -0400

    A blank demo_app cookbook

---
+++ cookbooks-sources/demo_app/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Mat Schaffer"
+maintainer_email "mat@mashion.net"
+license          "MIT"
+description      "Installs/Configures a demo ASP MVC3 app"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
