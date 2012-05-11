ab237f964650f71fb8933eab7a65a8a32725cf69
Mat Schaffer <mat@schaffer.me>
2012-05-11 17:05:13 -0400

    Include a name with bootstrap

--- script/bootstrap.ps1
+++ script/bootstrap.ps1
@@ -1,5 +1,6 @@
 knife bootstrap windows winrm `
-  ec2-174-129-55-195.compute-1.amazonaws.com `
+  ec2-184-72-146-96.compute-1.amazonaws.com `
   -x Administrator `
   -P xxxxx `
-  -r "role[demo_app]"
+  -r "role[demo_app]" `
+  -N app2
